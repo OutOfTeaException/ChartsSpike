cfe301c2cd6c66e8bf2cf8c4378c91213eb22854
Timo Kuper <timo.kuper@hec.de>
2020-01-02 15:13:32 +0100

    echarts: hübscher gemacht

--- src/app/echart/echart.component.ts
+++ src/app/echart/echart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EChartOption } from 'echarts';
 import { LuftdruckService } from '../luftdruck.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-echart',
@@ -21,39 +22,63 @@ export class EchartComponent implements OnInit {
 
   chartOption: EChartOption = {
     xAxis: {
-      type: 'category',
-      data: this.datumX,
+      type: 'time',
       axisLabel: {
-      formatter: function (value, index) {
-        // Formatted to be month/day; display year only in the first label
-        var date = new Date(value);
-        var texts = date.getDate() + "." + (date.getMonth() + 1) + " " + date.getHours() + ":" + date.getMinutes();
-        //if (index === 0) {
-            //texts.push(date.getYear());
-        //}
-        return texts;
+        formatter: (function(value){
+          return moment(value).format('DD.MM.YYYY HH:mm');
+      })
     }
-      }
+    //   formatter: function (value, index) {
+    //     // Formatted to be month/day; display year only in the first label
+    //     var date = new Date(value);
+    //     var texts = date.getDate() + "." + (date.getMonth() + 1) + " " + date.getHours() + ":" + date.getMinutes();
+    //     //if (index === 0) {
+    //         //texts.push(date.getYear());
+    //     //}
+    //     return texts;
+    // }
     },
     yAxis: [{
-      name: 'Luftdruck',
+      name: 'Luftdruck (mbar)',
       type: 'value',
-      //min: 800
+      scale: true,
+      nameRotate: 90,
+      nameLocation: 'middle',
+      nameGap: 50,
+      boundaryGap: ['10%', '10%'],
+      axisLabel: {
+      formatter:function (value, index) {
+        let n = new Number(value);
+        return n.toLocaleString();
+      }
+    }
     }, {
-      name: 'Temperatur',
+      name: 'Temperatur (°C)',
       type: 'value',
-     // min: 10
+      scale: true,
+      nameRotate: -90,
+      nameLocation: 'middle',
+      nameGap: 50,
+      boundaryGap: ['10%', '10%'],
+      axisLabel: {
+        formatter:function (value, index) {
+          let n = new Number(value);
+          return n.toLocaleString();
+        }
+      }
     }],
     series: [{
       name: 'Luftdruck',
-      data: this.luftdruckdatenY ,
-      type: 'line'
+      data: this.luftdruckdatenY,
+      type: 'line',
+      showSymbol: false,
     },
     {
       name: 'Temperatur',
       data: this.temperaturdatenY,
       type: 'line',
-      yAxisIndex: 1
+      yAxisIndex: 1,
+      showSymbol: false,
     }],
     legend: {
       data: ['Luftdruck','Temperatur']
@@ -75,6 +100,7 @@ export class EchartComponent implements OnInit {
   ],
   tooltip : {
     trigger: 'axis',
+    formatter: formatTooltip();
        // axisPointer: {
     //     type: 'cross',
     //     animation: false,
@@ -85,13 +111,42 @@ export class EchartComponent implements OnInit {
 },
 }
   
+function(params) {
+
+      if (!params[0] && !params[0].data) {
+        return;
+      }
+
+      let datum = params[0].data[0];
+      let tooltip = `Datum: ${moment(datum).format('DD.MM.YYYY HH:mm')}<br/>`;
+
+      if (params[0].data[1]) {
+        setToolTip(params[0]);
+        tooltip += "<br/>";
+      }
+      
+      if (params[1] && params[1].data[1]) {
+        setToolTip(params[1]);
+      }
+
+      function setToolTip(series) {
+        if (series.seriesName == "Luftdruck") {
+          tooltip += `Luftdruck: ${series.data[1].toLocaleString()} mbar`;
+        }
+        else if (series.seriesName == "Temperatur") {
+          tooltip += `Temperatur: ${series.data[1].toLocaleString()} °C`;
+        }
+      }
+      
+      return tooltip;
+    }
 
   showSensordaten() {
-    let sensordata = this.luftdruckService.getMockSensordaten(100);
+    let sensordata = this.luftdruckService.getSensordaten();
 
     sensordata.forEach(s => {
-      this.luftdruckdatenY.push(s.luftdruck);
-      this.temperaturdatenY.push(s.temperatur);
+      this.luftdruckdatenY.push([s.timestamp, s.luftdruck]);
+      this.temperaturdatenY.push([s.timestamp, s.temperatur]);
       this.datumX.push(s.timestamp);
     });
  }
